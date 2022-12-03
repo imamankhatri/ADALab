d70f5ac426ba5a0e4ba3031eb20c07f61dbca958
Mrunal Khatri <imamankhatri@gmail.com>
2022-12-03 23:46:28 +0530

    init

--- pg1.java
+++ pg1.java
@@ -23,6 +23,7 @@ public class pg1 {
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// test commit
 		int i,n;
 		System.out.println("Enter n");
 		Scanner in = new Scanner(System.in);
